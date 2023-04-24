9e1a36231e4ee7635ad1590ce782d2d461bae2ac
blackzarifa <rafaelirades@gmail.com>
2023-04-24 11:29:35 -0300

    Update EstudanteDAO.java
    Commented old code, will update from using the Statement class to use PreparedStatement

--- src/main/java/teste/persistence/EstudanteDAO.java
+++ src/main/java/teste/persistence/EstudanteDAO.java
@@ -2,18 +2,31 @@ package teste.persistence;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class EstudanteDAO {
 	
-	private static Connection conn = null;
-	private static Statement stmt = null;
+	Connection conn = null;
+	PreparedStatement pstm = null;
 	
+	
+	// Connect to database
+	private Connection getConnection() throws SQLException {
+		Connection conn;
+		conn = SqlConnection.getInstance().getConnection();
+		return conn;
+	}
+	
+	
+	
+	
+	
+	/*
 	public static void setConnection(Connection connection) {
 		conn = connection;
 		
 		try {
-			stmt = conn.createStatement();
+			pstm = conn.createStatement();
 		} catch (SQLException e) {
 			System.err.println("Erro criando a declaração.");
 			e.printStackTrace();
@@ -22,29 +35,24 @@ public class EstudanteDAO {
 	
 	public static void execute(String query) {
 		try {
-			stmt.execute(query);
+			pstm.execute(query);
 		} catch (SQLException e) {
 			System.err.println("Erro executando a query.");
 			e.printStackTrace();
 		}
 		System.out.println("Query executada.");
 	}
+	*/
 	
 	
 	
 	// main para testes
-	public static void main(String[] args) {
-		Connection conn = null;
-		try {
-			conn = SqlConnection.getConnection();
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
-		setConnection(conn);
-		String query = "select * from curso";
-		execute(query);
-	}
+	/*
+	 * public static void main(String[] args) { Connection conn = null; try { conn =
+	 * SqlConnection.getConnection(); } catch (SQLException e) {
+	 * System.err.println(e.getMessage()); e.printStackTrace(); }
+	 * 
+	 * setConnection(conn); String query = "select * from curso"; execute(query); }
+	 */
 	
 }
