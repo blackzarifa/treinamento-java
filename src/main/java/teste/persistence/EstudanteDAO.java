f4187524652f57d9361e8a3d88f1a707d1776422
blackzarifa <rafaelirades@gmail.com>
2023-04-24 12:34:04 -0300

    Created add function, simplified getConnection

--- src/main/java/teste/persistence/EstudanteDAO.java
+++ src/main/java/teste/persistence/EstudanteDAO.java
@@ -12,14 +12,41 @@ public class EstudanteDAO {
 	
 	// Connect to database
 	private Connection getConnection() throws SQLException {
-		Connection conn;
-		conn = SqlConnection.getInstance().getConnection();
-		return conn;
+		return SqlConnection.getInstance().getConnection();
 	}
 	
 	
-	
-	
+	// Add to database
+	public void add(EstudanteBean estudanteBean) {		
+		try {
+			String queryString = "INSERT INTO estudente(id, nome, matricula, aniversario, FK_curso) VALUES(?,?,?,?,?)";
+			
+			conn = getConnection();
+			pstm = conn.prepareStatement(queryString);
+			
+			pstm.setInt(1, estudanteBean.getId());
+			pstm.setString(2, estudanteBean.getNome());
+			pstm.setString(3, estudanteBean.getMatricula());
+			pstm.setDate(4, estudanteBean.getAniversario());
+			pstm.setInt(5, estudanteBean.getFK_curso());
+			
+			pstm.executeUpdate();
+			System.out.println("Estudante adicionado com sucesso.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	/*
 	public static void setConnection(Connection connection) {
