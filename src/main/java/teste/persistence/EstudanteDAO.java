80d5fa979629c6a45ff73b9ca82b533b07b30b05
blackzarifa <rafaelirades@gmail.com>
2023-04-24 09:47:07 -0300

    Renamed SqlStatement to EstudanteDAO

--- src/main/java/teste/persistence/EstudanteDAO.java
+++ src/main/java/teste/persistence/EstudanteDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SqlStatement {
+public class EstudanteDAO {
 	
 	private static Connection conn = null;
 	private static Statement stmt = null;
