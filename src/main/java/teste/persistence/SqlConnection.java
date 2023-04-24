a82ef410da5aaacede3b9ac9f377e50e32248a7c
blackzarifa <rafaelirades@gmail.com>
2023-04-24 11:00:22 -0300

    Update SqlConnection.java

--- src/main/java/teste/persistence/SqlConnection.java
+++ src/main/java/teste/persistence/SqlConnection.java
@@ -9,33 +9,55 @@ public class SqlConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/treinamento";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "passwd";
+	private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 	
-	public static Connection getConnection() throws SQLException {
-		Connection conn = null;
-		
+	private static SqlConnection sqlConnection = null; 
+	
+	
+	private SqlConnection() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			Class.forName(DRIVER_CLASS_NAME);
 		} catch (ClassNotFoundException e) {
 			System.err.println("Driver do banco de dados não encontrado.");
 			e.printStackTrace();
 		}
+	}
+	
+	public static SqlConnection getInstance() {
+		if (sqlConnection == null)
+			sqlConnection = new SqlConnection();
 		
+		return sqlConnection;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 		return conn;
 	}
 	
-	
-	/*
-	// main para testes
-	public static void main(String[] args) {
+	public void closeConnection(Connection conn) {
 		try {
-			Connection conn = getConnection();
-			if (conn != null) 
-				System.out.println("Sucesso!");
+			if (conn != null) {
+				conn.close();
+				System.out.println("Conexão encerrada.");
+			}
 		} catch (SQLException e) {
-			System.err.println(e.getMessage());
+			System.err.println("Erro ao fechar a conexão.");
 			e.printStackTrace();
 		}
 	}
-	*/
+		
+	
+	// main para testes
+	/*
+	 * public static void main(String[] args) { SqlConnection sqlConn =
+	 * SqlConnection.getInstance(); Connection conn = null;
+	 * 
+	 * try { conn = sqlConn.getConnection();
+	 * System.out.println("Conexão estabelecida com sucesso."); } catch
+	 * (SQLException e) {
+	 * System.err.println("Erro ao conectar com o banco de dados.");
+	 * e.printStackTrace(); } finally { sqlConn.closeConnection(conn); } }
+	 */
+	 
 }
