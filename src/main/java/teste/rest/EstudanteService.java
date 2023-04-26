a95a843822879d84e6f4d415b832bb0fe88dd85f
blackzarifa <rafaelirades@gmail.com>
2023-04-26 09:39:43 -0300

    Created test

--- src/main/java/teste/rest/EstudanteService.java
+++ src/main/java/teste/rest/EstudanteService.java
@@ -1,10 +1,28 @@
 package teste.rest;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import teste.persistence.EstudanteDAO;
 
 @Path("/estudantes")
 public class EstudanteService {
 	
+	private EstudanteDAO dao;
 	
+	public EstudanteService() {
+		dao = new EstudanteDAO();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String test() {
+		return "TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST";
+	}
 	
 }
