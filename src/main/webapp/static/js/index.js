b44bb8febe7478fd97b2a1dec2f1eec8b6e8c956
blackzarifa <rafaelirades@gmail.com>
2023-04-27 12:32:04 -0300

    Created get function

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,3 +1,16 @@
 // Load Axios
 const axios = require('axios');
 
+// GET "estudantes"
+axios.get('/rest/estudantes')
+    .then(response => {
+        const estudantes = response.data;
+
+        const table = document.getElementById('estudantes-table');
+        estudantes.forEach(estudante => {
+            let row = document.createElement('tr');
+            row.innerHTML = '<td>' + estudante.id + '</td>';
+            table.appendChild(row);
+        });
+    })
+    .catch(e => console.log(e));
