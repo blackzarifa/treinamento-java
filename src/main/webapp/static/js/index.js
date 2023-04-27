d2e65d575f2c41a7e40caad09a2ac311d59aa6a7
blackzarifa <rafaelirades@gmail.com>
2023-04-27 12:59:35 -0300

    Tried to fix date format, still needs more work

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -5,11 +5,16 @@ axios.get('http://localhost:8080/treinamento-java/rest/estudantes')
 
         const table = document.getElementById('estudantes-table');
         estudantes.forEach(estudante => {
+            // Convert date to a readable format
+            let date = new Date(estudante.aniversario);
+            formattedDate = data.toLocaleDateString('en-GB');
+
+            // Create object row
             let row = document.createElement('tr');
             row.innerHTML = '<td>' + estudante.id + '</td>' +
                             '<td>' + estudante.nome + '</td>' +
                             '<td>' + estudante.matricula + '</td>' +
-                            '<td>' + estudante.aniversario + '</td>' +
+                            '<td>' + formattedDate + '</td>' +
                             '<td>' + estudante.FK_curso + '</td>';
             table.appendChild(row);
         });
