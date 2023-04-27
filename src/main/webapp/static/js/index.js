0083638d40bdcf53152e08eca214bd8b9ce265a3
blackzarifa <rafaelirades@gmail.com>
2023-04-27 12:49:08 -0300

    Added the rest of the columns for the get function

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -6,7 +6,11 @@ axios.get('http://localhost:8080/treinamento-java/rest/estudantes')
         const table = document.getElementById('estudantes-table');
         estudantes.forEach(estudante => {
             let row = document.createElement('tr');
-            row.innerHTML = '<td>' + estudante.id + '</td>';
+            row.innerHTML = '<td>' + estudante.id + '</td>' +
+                            '<td>' + estudante.nome + '</td>' +
+                            '<td>' + estudante.matricula + '</td>' +
+                            '<td>' + estudante.aniversario + '</td>' +
+                            '<td>' + estudante.FK_curso + '</td>';
             table.appendChild(row);
         });
     })
