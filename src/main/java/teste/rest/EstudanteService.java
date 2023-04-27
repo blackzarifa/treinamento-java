267a76e65f9e5500bb1be8c4e9c6d9063e4d8bad
blackzarifa <rafaelirades@gmail.com>
2023-04-27 10:49:12 -0300

    Created getAll function

--- src/main/java/teste/rest/EstudanteService.java
+++ src/main/java/teste/rest/EstudanteService.java
@@ -1,5 +1,8 @@
 package teste.rest;
 
+import teste.persistence.EstudanteDAO;
+import teste.entity.EstudanteBean;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,7 +11,8 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import teste.persistence.EstudanteDAO;
+import java.util.List;
+
 
 @Path("/estudantes")
 public class EstudanteService extends superRest {
@@ -20,4 +24,10 @@ public class EstudanteService extends superRest {
 	}
 	
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<EstudanteBean> getAll() {
+		List<EstudanteBean> estudantes = dao.findAll();
+		return estudantes;
+	}
 }
