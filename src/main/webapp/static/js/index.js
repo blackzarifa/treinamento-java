f09f49fc0fbb6bcf26a7888b42db0d62827bfc84
blackzarifa <rafaelirades@gmail.com>
2023-04-28 09:22:47 -0300

    Now using momentJs for dates

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -5,16 +5,15 @@ axios.get('http://localhost:8080/treinamento-java/rest/estudantes')
 
         const table = document.getElementById('estudantes-table');
         estudantes.forEach(estudante => {
-            // Convert date to a readable format
-            let date = new Date(estudante.aniversario);
-            formattedDate = data.toLocaleDateString('en-GB');
+            // Convert date to a more readable format
+            let dataAniversario = moment(estudante.aniversario).format('DD/MM/YYYY');
 
             // Create object row
             let row = document.createElement('tr');
             row.innerHTML = '<td>' + estudante.id + '</td>' +
                             '<td>' + estudante.nome + '</td>' +
                             '<td>' + estudante.matricula + '</td>' +
-                            '<td>' + formattedDate + '</td>' +
+                            '<td>' + dataAniversario + '</td>' +
                             '<td>' + estudante.FK_curso + '</td>';
             table.appendChild(row);
         });
