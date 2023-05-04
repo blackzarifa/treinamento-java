c54538d08e4271267dcf968c5d6edb2f4544d909
blackzarifa <rafaelirades@gmail.com>
2023-05-04 12:34:16 -0300

    Create tests for teste.rest

---
+++ src/test/java/teste/rest/CursoServiceTest.java
@@ -0,0 +1,14 @@
+package teste.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
