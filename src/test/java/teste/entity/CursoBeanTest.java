75ca19c76127c53b7c22ea4984d54f3655722b92
blackzarifa <rafaelirades@gmail.com>
2023-05-05 12:42:35 -0300

    Created testGettersAndSetters

--- src/test/java/teste/entity/CursoBeanTest.java
+++ src/test/java/teste/entity/CursoBeanTest.java
@@ -3,6 +3,7 @@ package teste.entity;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,19 @@ class CursoBeanTest {
 	@BeforeAll
 	static void setUp() {
 		System.out.println("Starting tests.");
+	}
+	
+	@Test
+	void testGettersAndSetters() {
+		CursoBean curso = new CursoBean();
+		curso.setId(100);
+		curso.setNome("test");
+		
+		Assertions.assertAll(
+				"GettersAndSetters",
+				() -> assertEquals(100, curso.getId()),
+				() -> assertEquals("test", curso.getNome())
+			);
 	}
 	
 	@AfterAll
