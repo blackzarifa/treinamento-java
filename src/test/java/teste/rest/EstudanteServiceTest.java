d4b1ed9962ff36f877794906a2a2cb637e08e2f9
blackzarifa <rafaelirades@gmail.com>
2023-05-05 10:51:01 -0300

    Created methods for before and after tests

--- src/test/java/teste/rest/EstudanteServiceTest.java
+++ src/test/java/teste/rest/EstudanteServiceTest.java
@@ -2,13 +2,28 @@ package teste.rest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EstudanteServiceTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	EstudanteService estudanteService;
+	
+	@BeforeAll
+	static void setUp() {
+		System.out.println("Starting tests.");
+	}
+	
+	@BeforeEach
+	void init() {
+		estudanteService = new EstudanteService();
+	}
+	
+	@AfterAll
+	static void tearDown() {
+		System.out.println("Finished tests.");
 	}
 
 }
