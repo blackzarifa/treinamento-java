6e797f5f268bfbec0746711b4089d2227dcab768
blackzarifa <rafaelirades@gmail.com>
2023-05-05 12:40:39 -0300

    Create CursoBeanTest.java

---
+++ src/test/java/teste/entity/CursoBeanTest.java
@@ -0,0 +1,14 @@
+package teste.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoBeanTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
