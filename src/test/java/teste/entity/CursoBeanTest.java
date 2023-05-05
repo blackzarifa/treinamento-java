ed2187309db83a8aacd4b3047b4be74978cfedf5
blackzarifa <rafaelirades@gmail.com>
2023-05-05 12:41:11 -0300

    Created BeforeAll and AfterAll

--- src/test/java/teste/entity/CursoBeanTest.java
+++ src/test/java/teste/entity/CursoBeanTest.java
@@ -2,13 +2,20 @@ package teste.entity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CursoBeanTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeAll
+	static void setUp() {
+		System.out.println("Starting tests.");
+	}
+	
+	@AfterAll
+	static void tearDown() {
+		System.out.println("Finished tests.");
 	}
 
 }
