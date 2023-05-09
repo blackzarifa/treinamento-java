d95d64f89ae005c275316307d716f15e890bff4c
blackzarifa <rafaelirades@gmail.com>
2023-05-09 10:12:35 -0300

    Update constructors

--- src/main/java/teste/entity/EstudanteBean.java
+++ src/main/java/teste/entity/EstudanteBean.java
@@ -20,18 +20,22 @@ public class EstudanteBean implements Serializable {
 	private boolean presencial;
 	
 	public EstudanteBean() {}
-	public EstudanteBean(String nome, String matricula, Date aniversario, int FK_curso) {
+	public EstudanteBean(String nome, String matricula, Date aniversario, int FK_curso, int semestre, boolean presencial) {
 		this.setNome(nome);
 		this.setMatricula(matricula);
 		this.setAniversario(aniversario);
 		this.setFK_curso(FK_curso);
+		this.setSemestre(semestre);
+		this.setPresencial(presencial);
 	}
-	public EstudanteBean(int id, String nome, String matricula, Date aniversario, int FK_curso) {
+	public EstudanteBean(int id, String nome, String matricula, Date aniversario, int FK_curso, int semestre, boolean presencial) {
 		this.setId(id);
 		this.setNome(nome);
 		this.setMatricula(matricula);
 		this.setAniversario(aniversario);
 		this.setFK_curso(FK_curso);
+		this.setSemestre(semestre);
+		this.setPresencial(presencial);
 	}
 
 
